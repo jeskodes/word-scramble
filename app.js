c26a635327aa814d2c6a7193ff51b94f5d1070f7
jesk <86005120+jeskodes@users.noreply.github.com>
2023-08-25 07:00:25 +0100

    Add start btn, words array + click listener

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-//select element on html page use querySelector - selecting gameArea class//
-const gameArea = document.querySelector(".gameArea");
-console.log("gameArea");
+//select element on html page use querySelector - selecting gameArea class //
+//gameArea "object"//
+const gameArea = document.querySelector('.gameArea');
+
+///create a button to start - can also create btn in hmtl
+//document.createElement() is a method. 
+const btn = document.createElement('button');
+btn.textContent = "START GAME";
+
+///Add to HTML page using append.
+gameArea.append(btn);
+
+console.log(btn);
+
+///creating array of words to scramble
+const myWords = ["bird", "dog", "cat", "cow"];
+
+///Add in event listener - listening for presses on the btn.
+///the button is a useable object. 
+///The event object - can write out as function ?(e)=>{}
+btn.addEventListener('click',(e)=>{
+    console.log(e);
+})
