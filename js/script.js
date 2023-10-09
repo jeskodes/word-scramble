ffa92f542db013913b35b836285b29b72d7a9005
jesk <86005120+jeskodes@users.noreply.github.com>
2023-10-09 07:40:06 +0100

    Rename restart button not working

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ output.classList.add("div-played-words");
 
 const restart = document.createElement('button');//EDIT Add refresh button
 restart.id = "btn-refresh";
-restart.innerHTML = "Restart"; 
+restart.innerHTML = "Restart-button"; 
 document.body.appendChild(restart);
 console.log(restart); //test restart button 
 
