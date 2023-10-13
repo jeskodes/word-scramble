9cd93ea3566b5a5eaa43040aa7bb07c592067a70
jesk <86005120+jeskodes@users.noreply.github.com>
2023-10-13 07:06:37 +0100

    Add css to folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -266,12 +266,15 @@ inWord.addEventListener('keyup', (e) => {  //make more accessible if change from
     // console.log(e); removed - used for debugging
     // inWord.style.borderColor = "#000000"; //reset border color to default
     // inWord.style.borderWidth = "2px"; //keep border width same throughout game play
-    if (inWord.value.length == game.sel.length && e.code == "Enter") {
+    if (inWord.value.length == game.sel.length || e.code == "Enter") {
         winChecker(); //runt the winChecker function 
     }
 
 })
 
+
+//if (inWord.value.length == game.sel.length && e.code == "Enter") { changed 
+// from or to and - to change back. 
 // btn.addEventListener ('keyup', (e) => {
 //     if (e.key === "Enter" || e.code === "Enter")
 //         document.getElementsByTagName('btn-start').click(); { 
